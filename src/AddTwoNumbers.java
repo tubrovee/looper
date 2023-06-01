9c071a64883ca573fe9fb581395ff850956c636d
turbovee <vishal.sfo@gmail.com>
2023-06-01 16:40:35 -0700

    fixed typos code

--- src/AddTwoNumbers.java
+++ src/AddTwoNumbers.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 public class AddTwoNumbers {
     public static void main(String[] args){
         Scanner myScanner=new Scanner(System.in);
-        Integer firstnomber = myScanner.nextInt();
-        Integer secondnumber = myScanner.nextInt();
-        System.out.println(firstnomber+secondnumber);
+        Integer first_number = myScanner.nextInt();
+        Integer second_number = myScanner.nextInt();
+        System.out.println(first_number + second_number);
 
 
     }
